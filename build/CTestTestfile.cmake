5b43dc48a6432b72d351b21f5259b35519ee2a09
Travis Aucoin <crowstrum@gmail.com>
2020-08-31 23:36:21 -0500

    removed build folder

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/travi/Documents/Development/SFML_Learning
-# Build directory: C:/Users/travi/Documents/Development/SFML_Learning/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/sfml-build")
--- build/_deps/sfml-build/src/SFML/Main/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/travi/Documents/Development/SFML_Learning/build/_deps/sfml-src/src/SFML/Main
-# Build directory: C:/Users/travi/Documents/Development/SFML_Learning/build/_deps/sfml-build/src/SFML/Main
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
